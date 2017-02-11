b924a8565bfeb57f32d6f02cc454e0fa556760fd
xp <xpeiro92@gmail.com>
2017-02-11 21:21:21 +0100

    trash erased

--- first_steps/load_libraries.py
+++ first_steps/load_libraries.py
@@ -12,5 +12,3 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
-
-print "hello"
